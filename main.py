db639970a2811e38de375b691c61cce8013a5c98
scherzono1 <cgrafa96@gmail.com>
2019-02-28 17:29:59 +0100

    wrote an automata format for files and implemented the reading of such files in the main.py

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
-#main function
-#git branch test
+import numpy as np
+def readAutomata(f):
+    global states,inputs
+    #deletes comments in file
+    forg = open("automata.mat","r")
+    for s in forg:
+        if(s[0]!='#'): 
+            f.write(s)
+    f.close()
+    f = open(".automata.mat","r")
+  
+    number_inputs = int (f.readline())
+    number_states = int (f.readline())
+    
+    #reads possible inputs of automata  
+    inputs_line = f.readline()
+    for i in range ( number_inputs ):
+        inputs.append( inputs_line[i] )
+    print(inputs)
+    
+    #reads possible states of automata
+    states_line = f.readline()
+    for i in range ( number_states ):
+        states.append( states_line[i] )
+    print(states)
+
+    #fills automata_matrix
+    mat = np.chararray((number_states,number_inputs), unicode = True)
+    for i in range (number_states):
+        s = f.readline()
+        for j in range ( number_inputs ):
+                mat[i][j] = s[j]
+    return mat
+    
+#main
+states = []
+inputs = []
+f = open(".automata.mat","w")
+automata = readAutomata(f)
+print(automata)
