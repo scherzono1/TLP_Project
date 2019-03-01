7535c409c9364676016411930cff042bb51c6fb1
scherzono1 <cgrafa96@gmail.com>
2019-03-01 15:45:02 +0100

    program now fully reads from text file and then checks that input word belongs to said automata!

--- main.py
+++ main.py
@@ -30,9 +30,55 @@ def readAutomata(f):
                 mat[i][j] = s[j]
     return mat
     
+def drawAutomata(states,inputs,automata):
+    #function to draw an automata. to me able to vizualize it
+    return 0
+
+def testAutomata(automata,states,inputs,final_states):
+    accepted = True
+    input_string =  str( input() )  
+    for i in input_string:
+        if (belongs_to_list(i,inputs) == False):
+            return False
+    ans = search_automata(automata,states,input_string,0,0,len(input_string)-1,final_states)
+    return ans
+        
+
+def search_automata(automata,states,inputs,s,i,max_i,final_states):
+    cur_input = int( inputs[i] )
+    if  i+1 > max_i: 
+            if belongs_to_list( str ( automata[s][cur_input] ) ,final_states )==True:
+                print('horray!')
+                return True
+            else: return False
+    if automata[s][cur_input] == '.': return False
+    s = states.index( automata[s][cur_input] )
+    return search_automata(automata,states,inputs,s,i+1,max_i,final_states)
+    
+
+def belongs_to_list(char, test):
+    belongs = False
+    for c in test:
+        if (c == char): 
+            belongs = True
+    return belongs
+
+        
+
 #main
 states = []
 inputs = []
+final_states = ['d']
 f = open(".automata.mat","w")
 automata = readAutomata(f)
+print('inputs:')
+print(inputs)
+print('states:')
+print(states)
+print('automata:')
 print(automata)
+print('Write down a word to test the automata:')
+if ( testAutomata(automata,states,inputs,final_states) == True):
+    print('it works!')
+else:
+    print('wrong')
